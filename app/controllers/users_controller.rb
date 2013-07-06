08a63bb6abff18f5b88e6c5d68aab7025a9bae80
Dave Ghijben <dghijben@gmail.com>
2013-07-06 11:00:34 +0900

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, 
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
   
   def show
     @user = User.find(params[:id])
+    @micropost = Micropost.new
     @microposts = @user.microposts.paginate(page: params[:page])
   end
   
@@ -44,6 +46,20 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "Bye Bye User"
     redirect_to users_url
+  end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
   
   private
